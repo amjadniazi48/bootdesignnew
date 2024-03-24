d716a5e1ac528740a9f071adc91335624cc11834
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2024-03-24 17:32:10 +0500

    Changes to get the url from online

--- app/config/index.tsx
+++ app/config/index.tsx
@@ -1,6 +1,6 @@
 export const API_URL =
 //  process.env.NEXT_PUBLIC_API_URL ||
 //  "https://strapi-production-9f68.up.railway.app";
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+  process.env.NEXT_PUBLIC_API_URL || "https://iktrust-production.up.railway.app";
 export const NEXT_URL =
   process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
