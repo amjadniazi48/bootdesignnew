83f9c9a14bb700c85fc839c5e676b9f3f0a8e0c0
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2024-06-08 16:31:28 +0500

    Added the margin right

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
               <li className="nav-item"><a className="nav-link" href="privacy-policy.html">خصوصية</a></li>
               <li className="nav-item"><a className="nav-link" href="#" target="_blank">اشتري الآن</a></li>
             </ul>   
-            <span style={{color:"white"}}>Copyright © 2024 IK Trust. All Rights Reserved. </span>
+            <span style={{color:"white",marginRight:"15%"}}>Copyright © 2024 IK Trust. All Rights Reserved. </span>
          
           </nav>
        
