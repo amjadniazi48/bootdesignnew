d2e7aeda909af627ce2c9762d2e3ec2190206b56
amjadniazi48 <amjadiqbalkhanniazi@gmail.com>
2025-09-29 20:07:14 +0500

    Sort the cat post by createdAt

--- app/aboutus/[cats]/page.js
+++ app/aboutus/[cats]/page.js
@@ -41,6 +41,7 @@ const HighLights = () => {
             },
           },
         },
+         sort: ["createdAt:desc"], // ✅ latest first
         pagination: {
           page,
           pageSize: limit,
